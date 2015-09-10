ea43b9c6feab6f1909a9c7b09637838c0b4e022a
Nick Golding <goldingn35@gmail.com>
2015-09-10 22:03:37 +0100

    lowering expectations (my own) ...

--- practicals/04_validation.R
+++ practicals/04_validation.R
@@ -3,8 +3,7 @@
 # The aim of this practical is to play with different ways of validating models
 # and see how they affect the different metrics and models. We'll cover
 # in-sample versus out-of-sample validation, k-fold validation and spatially-
-# stratified cross-validation. If time allows, we'll carry out a group
-# experiment comparing in- and out-of- sample validation statistics.
+# stratified cross-validation.
 
 # clear workspace
 rm(list = ls())
