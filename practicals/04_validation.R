9c4c63738ec06f8455a7469556e8b9884b694d5f
Nick Golding <goldingn35@gmail.com>
2015-09-11 07:53:22 +0100

    adding to 4th practical with examples of validation procedures

--- practicals/04_validation.R
+++ practicals/04_validation.R
@@ -5,5 +5,45 @@
 # in-sample versus out-of-sample validation, k-fold validation and spatially-
 # stratified cross-validation.
 
-# clear workspace
+# clear workspace and load zoon
 rm(list = ls())
+library(zoon)
+
+# ~~~~~~~~~~~~
+# In-sample validation statistics
+
+ModuleHelp(PerformanceMeasures)
+
+work1 <- workflow(occurrence = UKAnophelesPlumbeus,
+              covariate = UKAir,
+              process = OneHundredBackground,
+              model = LogisticRegression,
+              output = PerformanceMeasures)
+
+# ~~~~~~~~~~~~
+# (standard) k-fold cross-validation
+
+ModuleHelp(Crossvalidate)
+
+# vary number of folds and look at predictive performance each time
+work2 <- workflow(occurrence = UKAnophelesPlumbeus,
+                  covariate = UKAir,
+                  process = c(OneHundredBackground,
+                              Crossvalidate(k = 3)),
+                  model = LogisticRegression,
+                  output = PerformanceMeasures)
+
+
+# ~~~~~~~~~~~~
+# spatially stratified cross-validation
+
+ModuleHelp(PartitionDisc)
+
+# vary number of folds and look at predictive performance each time
+work3 <- workflow(occurrence = UKAnophelesPlumbeus,
+                  covariate = UKAir,
+                  process = c(OneHundredBackground,
+                              PartitionDisc),
+                  model = LogisticRegression,
+                  output = PerformanceMeasures)
+
